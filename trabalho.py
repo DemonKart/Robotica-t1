787d8e1a96ea71ca49f0dde875cd49e1e43e7d18
demonkart <alanpcsilva@gmail.com>
2015-11-18 14:15:00 +0000

    Python doesn't know how to crete an object e.e
    Signed-off-by: demonkart <alanpcsilva@gmail.com>

--- trabalho.py
+++ trabalho.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python
 from math import sqrt
 
+class Ponto(object):
+  x = 0
+  y = 0
+  t = 0.0
+  
+  def __init__(self, x, y, t):
+        self.x = x
+        self.y = y
+        self.t = t
+
 text_file = open("in.txt", "r")
 lines = text_file.read().split('\n')
-print lines
+
+pontos = []
+
+for i in range(len(lines)):                                                                                                                                                                                   
+  dados = lines[i].split(',')
+  print dados
+  p = Ponto(lines[0], lines[1], lines[2])
+  pontos.append(p)
+  
+for p in pontos:
+  print "p = (" + p.x + "," + p.y + "), " + p.t
+
 text_file.close()
