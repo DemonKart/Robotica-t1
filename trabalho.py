e2762af202da59ac5e0c41a080e50937417e82a4
demonkart <alanpcsilva@gmail.com>
2015-11-19 14:17:19 +0000

    Trying to fix angle calc
    Signed-off-by: demonkart <alanpcsilva@gmail.com>

--- trabalho.py
+++ trabalho.py
@@ -1,16 +1,47 @@
 #!/usr/bin/env python
 from math import sqrt
+from math import atan
+from math import degrees
 
 class Ponto(object):
-  x = 0
-  y = 0
+  x = 0.0
+  y = 0.0
   t = 0.0
+  v = 0.0
+  a = 0.0
   
-  def __init__(self, x, y, t):
+  def __init__(self, x, y, t, v, a):
         self.x = x
         self.y = y
         self.t = t
+        self.v = 0.0
+        self.a = 0.0
 
+def velocidade(p1, p2):
+  deltaX = p2.x-p1.x
+  deltaY = p2.y-p1.y
+  deltaT = p2.t-p1.t
+  
+  deltaS = sqrt(abs((deltaY^2)-(deltaX^2))) # Calculo de velocidade media
+  return (deltaS/deltaT)
+  
+def angulo(p1, p2):
+  deltaX = p2.x-p1.x
+  deltaY = p2.y-p1.y
+
+  if deltaX == 0: # Se nao se deslocou no eixo X, divisao por zero!
+    if deltaY > 0: # Se deslocou em Y para cima
+      return 90.0
+    if deltaY < 0: # Se deslocou em Y para baixo
+      return -90.0
+    return p1.a # Nao se deslocou nem em X nem em Y
+    
+  m = deltaY/deltaX
+  a = degrees(atan(m))
+    
+  return a # Se deslocou em X e Y
+  
+  
 text_file = open("in.txt", "r")
 lines = text_file.read().split('\n')
 
@@ -18,12 +49,15 @@ pontos = []
 
 for i in range(len(lines)):                                                                                                                                                                                   
   dados = lines[i].split(',')
-  p = Ponto(dados[0], dados[1], dados[2])
+  p = Ponto(int(dados[0]), int(dados[1]), float(dados[2]),0.0,0.0)
+  if i > 0: # Se estiver a partir do segundo ponto
+    p.v = velocidade(pontos[i-1], p)
+    p.a = angulo(pontos[i-1], p)
+    
   pontos.append(p)
   
-for p in pontos:
-  print "p = (" + p.x + "," + p.y + "), " + p.t
-  
-
+for i in range(len(lines)):
+  p = pontos[i]
+  print "P" + str(i) + "= (" + str(p.x) + "," + str(p.y) + ") \t| Tempo " + str(p.t) + "\t | Angulo " + str(p.a) + "\t | Velocidade=> Total: " + str("%0.4f" % p.v) + " \t Cada roda: " + str("%0.4f" % ((p.v)/2))
 
 text_file.close()
